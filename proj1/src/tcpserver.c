07d30096894e3f5885855eab74d9afafb751abb9
mattandreas24 <matta@udel.edu>
2018-03-20 22:38:25 -0400

    Update tcpserver.c
    update

--- proj1/src/tcpserver.c
+++ proj1/src/tcpserver.c
@@ -1,114 +1,141 @@
-/* tcp_ client.c */
-/* Programmed by Matt Andreas and Monica Mooney */
-/* March 22, 2018 */
+/* tcpserver.c */
+/* Programmed by Adarsh Sethi */
+/* February 21, 2018 */
 
-#include <netdb.h>          /* for struct hostent and gethostbyname */
+#include <ctype.h>          /* for toupper */
 #include <stdio.h>          /* for standard I/O functions */
 #include <stdlib.h>         /* for exit */
-#include <string.h>         /* for memset, memcpy, and strlen */
-#include <sys/socket.h>     /* for socket, connect, send, and recv */
-#include <sys/types.h>
-#include <sys/unistd.h>
-
+#include <string.h>         /* for memset */
+#include <sys/socket.h>     /* for socket, bind, listen, accept */
+#include <netinet/in.h>     /* for sockaddr_in */
+#include <unistd.h>         /* for close */
+#include "datastruct.h"
 
 #define STRING_SIZE 1024
-#define HOSTNAME "cisc450.cis.udel.edu" /* Server's hostname */
-#define PORT_NUM 46464 /* Port number used by server (remote port) */
+
+/* SERV_TCP_PORT is the port number on which the server listens for
+   incoming requests from clients. You should change this to a different
+   number to prevent conflicts with others in the class. */
+
+#define SERV_TCP_PORT 46464
 
 int main(void) {
 
-	int sock_client;  /* Socket used by client */
+	int sock_server;  /* Socket on which server listens to clients */
+	int sock_connection;  /* Socket on which server exchanges data with client */
 
 	struct sockaddr_in server_addr;  /* Internet address structure that
                                         stores server address */
-	struct hostent * server_hp;      /* Structure to store server's IP
-                                        address */
-	char filename[STRING_SIZE];  /* send message */
-	char line_fromfile[STRING_SIZE]; /* receive message */
+	unsigned int server_addr_len;  /* Length of server address structure */
+	unsigned short server_port;  /* Port number used by server (local port) */
+
+	struct sockaddr_in client_addr;  /* Internet address structure that
+                                        stores client address */
+	unsigned int client_addr_len;  /* Length of client address structure */
+
+	char sentence[STRING_SIZE];  /* receive message */
+	char modifiedSentence[STRING_SIZE]; /* send message */
 	int bytes_sent, bytes_recd; /* number of bytes sent or received */
+	unsigned int i;  /* temporary loop variable */
 
-	/* OPEN SOCKET */
+	/* open a socket */
 
-	if ((sock_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-		perror("Client: can't open stream socket");
+	if ((sock_server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("Server: can't open stream socket");
 		exit(1);
 	}
 
-	/* Note: there is no need to initialize local client address information
-            unless you want to specify a specific local port
-            (in which case, do it the same way as in udpclient.c).
-            The local address initialization and binding is done automatically
-            when the connect function is called later, if the socket has not
-            already been bound. */
-
-	/* INITIALIZE SERVER:
-	 * address information */
-
-	printf("The server hostname is: %s \n", HOSTNAME);
-	if ((server_hp = gethostbyname(HOSTNAME)) == NULL) {
-		perror("Client: invalid server hostname");
-		close(sock_client);
-		exit(1);
-	}
-
-	printf("The server port number is: %d \n", PORT_NUM);
-
-
-	/* USER INTERFACE */
-
-	//Client prompts user for filename
-	printf("Please input a filename:\n");
-	scanf("%s", filename);
-
-	/* INITIALIZE SERVER:
-	 * clear server address structure and initialize with server address */
+	/* initialize server address information */
 
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	memcpy((char *)&server_addr.sin_addr, server_hp->h_addr,
-			server_hp->h_length);
-	server_addr.sin_port = htons(PORT_NUM);
+	server_addr.sin_addr.s_addr = htonl (INADDR_ANY);  /* This allows choice of
+                                        any host interface, if more than one
+                                        are present */
+	server_port = SERV_TCP_PORT; /* Server will listen on this port */
+	server_addr.sin_port = htons(server_port);
 
-	/* CONNECT TO SERVER */
-	printf("before if \n");
+	/* bind the socket to the local server port */
 
-	//Client sends a connection request
-	if (connect(sock_client, (struct sockaddr *) &server_addr,
+	if (bind(sock_server, (struct sockaddr *) &server_addr,
 			sizeof (server_addr)) < 0) {
-		perror("Client: can't connect to server");
-		close(sock_client);
+		perror("Server: can't bind to local address");
+		close(sock_server);
 		exit(1);
 	}
-	printf("after if \n");
 
-	/* SEND MESSAGE (the filename) */
+	/* listen for incoming requests from clients */
 
-	//When connection is established, client sends filename to server
-	bytes_sent = send(sock_client, filename, sizeof(filename), 0);
-
-	FILE *file;
-	file = fopen("output.txt", "w");
-	/* GET RESPONSE from server (the contents of the file) */
-	printf("response received \n");
-	if (file) {
-		do {
-			unsigned short header[STRING_SIZE];
-			printf("before recv statement \n");
-			bytes_recd = recv(sock_client, header, STRING_SIZE, 0);
-			printf("bytes_recd: %d \n", bytes_recd);
-			if (ntohs(header[1]) == 0)
-				break;
-			bytes_recd = recv(sock_client, line_fromfile, ntohs(header[1]), 0);
-			printf("Received line is:\n");
-			printf("%s \n", line_fromfile);
-		} while (fprintf(file, line_fromfile) > 0);
+	if (listen(sock_server, 50) < 0) {    /* 50 is the max number of pending */
+		perror("Server: error on listen"); /* requests that will be queued */
+		close(sock_server);
+		exit(1);
 	}
-	if (ferror(file)) {
-		/* deal with error */
-	}
-	printf("\nExiting\n");
+	printf("I am here to listen ... on port %hu\n\n", server_port);
 
+	client_addr_len = sizeof (client_addr);
+
+	/* wait for incoming connection requests in an indefinite loop */
+
+	for (;;) {
+
+		sock_connection = accept(sock_server, (struct sockaddr *) &client_addr,
+				&client_addr_len);
+		/* The accept function blocks the server until a
+                        connection request comes from a client */
+		if (sock_connection < 0) {
+			perror("Server: accept() error\n");
+			close(sock_server);
+			exit(1);
+		}
+
+		/* receive the message */
+		bytes_recd = recv(sock_connection, sentence, STRING_SIZE, 0);
+		if (bytes_recd > 0){
+			printf("Received file is:\n");
+			printf("%s", sentence);
+			printf("\nwith length %d\n\n", bytes_recd);
+			/* send message */
+			FILE *file;
+			file = fopen(sentence, "r");	// The file to read in
+			if (file) {
+				char * line;	// A pointer to the line that will be read in
+				unsigned short seqnum = 0;	// Sequence number of the packet to be sent
+				size_t linelen = 0;	// The length of the line to be read in
+				while (getline(&line, &linelen, file) > 0) {	
+					/* Getting the next line, continuously going until no more lines left */
+					printf("Read line is:\n");
+					printf("%s", line);
+					unsigned short header[2] = {htons((seqnum++)-1), htons((unsigned short) linelen)};
+					/* This header contains the sequence number in the [0] position and
+					 * the line length in the [1] position. Both of these consist of unsigned short
+					 * integers. The integers are converted to Network Order by the htons() function.
+					 * The header is now ready to be sent. */
+					bytes_sent = send(sock_connection, header, sizeof(header), 0);
+					/* The header is sent using the send() function to the client */
+					bytes_sent = send(sock_connection, line, linelen, 0);
+					/* Send the line read in */
+					printf("Sent line is:\n");
+					printf("%s", line);
+				} // No more lines
+				unsigned short header[2] = {htons((seqnum++)-1), htons(0)};
+				/* After all of the lines are read through, a header is sent
+				 * with a data-size of the next line as 0 indicating no more lines
+				 * left, and the sequence value is incremented */
+				bytes_sent = send(sock_connection, header, sizeof(header), 0);
+				/* Send the header, indicating communication is done */
+			}
+			printf("All lines sent\n");
+			if (ferror(file)) {	// If there is a file error
+				close(sock_connection); 
+				exit(1);
+				/* Close the connection and exit */
+			}
+			fclose(file);	// Close the file
+		}
+		break;	// Break out of the loop
+	}
 	/* close the socket */
-
-	close (sock_client);
+	close(sock_connection);
+	printf("Socket closed.\n");
 }
