5f2334fb65993f8d373befaf24592e19b2621b50
mattandreas24 <matta@udel.edu>
2018-04-02 13:54:54 -0400

    Update tcpclient.c
    Fully working client

--- proj1/src/tcpclient.c
+++ proj1/src/tcpclient.c
@@ -23,9 +23,6 @@ int main(void) {
                                         stores server address */
 	struct hostent * server_hp;      /* Structure to store server's IP
                                         address */
-	char filename[STRING_SIZE];  /* send message */
-	char line_fromfile[STRING_SIZE]; /* receive message */
-	int bytes_sent, bytes_recd; /* number of bytes sent or received */
 
 	/* OPEN SOCKET */
 
@@ -53,13 +50,6 @@ int main(void) {
 
 	printf("The server port number is: %d \n", PORT_NUM);
 
-
-	/* USER INTERFACE */
-
-	//Client prompts user for filename
-	printf("Please input a filename:\n");
-	scanf("%s", filename);
-
 	/* INITIALIZE SERVER:
 	 * clear server address structure and initialize with server address */
 
@@ -79,35 +69,64 @@ int main(void) {
 		exit(1);
 	}
 
+	/* USER INTERFACE */
+
+	//Client prompts user for filename
+	char filename[STRING_SIZE];
+	printf("Please input a filename:\n");
+	scanf("%s", filename);
+
 	/* SEND MESSAGE (the filename) */
 
+	int bytes_sent, bytes_recd; /* number of bytes sent or received */
+	int seqnum = 0, linelen = strlen(filename) + 1;
 	//When connection is established, client sends filename to server
-	bytes_sent = send(sock_client, filename, sizeof(filename), 0);
-
-	FILE *file;
+	unsigned short header[2] = {htons((unsigned short)seqnum), htons((unsigned short)linelen)};
+	/* This header contains the sequence number in the [0] position and
+	 * the line length in the [1] position. Both of these consist of unsigned short
+	 * integers. The integers are converted to Network Order by the htons() function.
+	 * The header is now ready to be sent. */
+	if ((bytes_sent = send(sock_client, header, sizeof(header), 0)) < 1 || (bytes_sent = send(sock_client, filename, linelen, 0)) < 1) {
+		printf("Error sending filename, exiting\n");
+		close (sock_client);
+		exit(1);
+	}
+	printf("Packet %d with %d data bytes sent for the filename\n", seqnum, linelen);
+	int totalbytes = 0;
+	FILE * file;
+	char line_fromfile[STRING_SIZE];	// A pointer to the line that will be read in
 	file = fopen("output.txt", "w");
 	/* GET RESPONSE from server (the contents of the file) */
 	if (file) {
 		do {
 			unsigned short header[2];
-			bytes_recd = recv(sock_client, header, 4, 0);
-			printf("bytes_recd for header: %d \n", bytes_recd);
-			header[0] = ntohs(header[0]);
-			header[1] = ntohs(header[1]);
-			if (header[1] == 0)
+			int prevseq;
+			if ((bytes_recd = recv(sock_client, header, 4, 0)) != 4 || (prevseq = seqnum) != ((seqnum = ntohs(header[0])) - 1) || (linelen = ntohs(header[1])) < 0) {
+				printf("Error on server side\n");
 				break;
-			bytes_recd = recv(sock_client, line_fromfile, header[1], 0);
-			printf("Received line is: %s \n", line_fromfile);
-			printf("With length: %d \n", header[1]);
-			printf("And sequence: %d \n\n", header[0]);
-		} while (fprintf(file, line_fromfile));
+			}
+			else if (linelen == 0) {
+				printf("End of Transmission Packet with sequence number %d received with %d data bytes\n", seqnum, linelen);
+				printf("Total number of packets received: %d\n", seqnum-1);
+				printf("Total number of data bytes received: %d\n", totalbytes);
+				break;
+			}
+			line_fromfile[linelen] = '\0';	// Declaring buffer size
+			if ((bytes_recd = recv(sock_client, line_fromfile, linelen, 0)) != linelen) {
+				printf("bytes_recd is different from linelen, error\n");
+				break;
+			}
+			printf("Packet %d with %d data bytes received\n", seqnum, linelen);
+			totalbytes += linelen;
+		} while (fputs(line_fromfile, file) > 0);
+		fclose(file);	// Close the file
 	}
-	if (ferror(file)) {
-		/* deal with error */
-	}
+	else if (ferror(file)) 	// If there is a file error
+		printf("Error with file\n");
 	printf("\nExiting\n");
 
 	/* close the socket */
 
 	close (sock_client);
 }
+
